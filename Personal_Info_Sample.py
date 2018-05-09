4091e9bda0d01a1043b74504ce5ecc485b3ff239
DrakeEvans <31104161+DrakeEvans@users.noreply.github.com>
2018-05-09 11:17:17 -0400

    Update Personal_Info_Sample.py

--- Personal_Info_Sample.py
+++ Personal_Info_Sample.py
@@ -1,3 +1,3 @@
 httpRequestURL = ""  #Get this URL from IFTTT when you set up the HTTPRequest trigger
-databaseFileLocation = r"~/Library/Messages/chat.db"
-handle_id = 7  #find this by exploring the database above with a sql viewer of some type
+databaseFileLocation = r"~/Library/Messages/chat.db" #Default location of the Messages.app databse file
+handle_id = 7  #find this by exploring the database above with an sql viewer of some type
